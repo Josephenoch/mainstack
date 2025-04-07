1826974435a140cf5720823e7415ae670d3b86bb
Joseph Enoch <enochjoseph01@gmail.com>
2025-04-07 03:34:14 +0100

    install: add degular-font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,77 @@
 import "@/styles/globals.css";
+import localFont from "next/font/local";
 import type { AppProps } from "next/app";
 
+const degular = localFont({
+  src: [
+    {
+      path: "../public/fonts/DegularDemo-Thin.otf",
+      weight: "100",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-ThinItalic.otf",
+      weight: "100",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Light.otf",
+      weight: "300",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-LightItalic.otf",
+      weight: "300",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Regular.otf",
+      weight: "400",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-Italic.otf",
+      weight: "400",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Medium.otf",
+      weight: "500",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-MediumItalic.otf",
+      weight: "500",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Semibold.otf",
+      weight: "600",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-SemiboldItalic.otf",
+      weight: "600",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Bold.otf",
+      weight: "700",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-BoldItalic.otf",
+      weight: "700",
+      style: "italic"
+    },{
+      path: "../public/fonts/DegularDemo-Black.otf",
+      weight: "900",
+      style: "normal"
+    },{
+      path: "../public/fonts/DegularDemo-BlackItalic.otf",
+      weight: "900",
+      style: "italic"
+    },
+
+  ],
+  variable: "--font-degular"
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${degular.variable} font-degular`}>
+      <Component {...pageProps} />
+    </main>
+  )
+ 
+
 }
