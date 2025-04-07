c2f3b8394d2b02c7a6d71af107124b71849a32ed
Joseph Enoch <enochjoseph01@gmail.com>
2025-04-07 12:13:36 +0100

    chore: fix shadow

--- layout/SideFloatingBar.tsx
+++ layout/SideFloatingBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SideFloatingBar = () => {
   return (
-    <div className="fixed shadow-[0px_6px_0px_#5C738314,0px_4px_8px_0px_#5C738314] rounded-full px-3 bg-white fixed-4 flex flex-col py-4 items-center space-y-6 top-1/2 -translate-y-1/2">
+    <div className="fixed shadow-[0px_6px_6px_0px_#5C738314,0px_4px_8px_0px_#5C738314] rounded-full px-3 bg-white fixed-4 flex flex-col py-4 items-center space-y-6 top-1/2 -translate-y-1/2">
       <Image  
         width={24}
         height={24}
